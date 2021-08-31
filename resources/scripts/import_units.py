340be1f962d74cdabf2441a685e3b5dbcaac9ce4
Tanaes <jonsan@gmail.com>
2021-08-30 23:29:33 -0400

    bug

--- resources/scripts/import_units.py
+++ resources/scripts/import_units.py
@@ -82,7 +82,7 @@ def import_df(df_path, match_column, seq_dirs, name_column,
     with open(units_fp, 'w') as f:
         f.write('Sample\tUnit\tR1\tR2\n')
         for sample, unit, r1, r2 in seq_tuples:
-            f.write('%s\n' % '\t'.join(sample, unit, r1, r2))
+            f.write('%s\n' % '\t'.join([sample, unit, r1, r2]))
 
 
 if __name__ == '__main__':
